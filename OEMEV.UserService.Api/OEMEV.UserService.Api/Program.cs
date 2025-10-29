ff24482a72b3ba2833befe02c5db2023d16bf978
chaulb <chouladila@gmail.com>
2025-10-29 17:52:30 +0700

    chore: deploy

--- OEMEV.UserService.Api/OEMEV.UserService.Api/Program.cs
+++ OEMEV.UserService.Api/OEMEV.UserService.Api/Program.cs
@@ -135,11 +135,15 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
 	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+	app.UseSwaggerUI(c =>
+{
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "OEMEV UserService API v1");
+	c.RoutePrefix = string.Empty;
+});
+//}
 
 app.UseHttpsRedirection();
 
