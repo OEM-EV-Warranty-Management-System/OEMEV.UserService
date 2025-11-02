d41e420b48de71a44577a27b8172f95eda3c2c30
chaulb <chouladila@gmail.com>
2025-11-02 19:59:43 +0700

    feat: Refresh token

--- OEMEV.UserService.Api/OEMEV.UserService.Application/Interfaces/IUserService.cs
+++ OEMEV.UserService.Api/OEMEV.UserService.Application/Interfaces/IUserService.cs
@@ -5,6 +5,7 @@ namespace OEMEV.UserService.Application.Interfaces
 	public interface IUserService
 	{
 		Task<Result<LoginResponseDto>> LoginAsync(LoginRequestDto loginRequestDto);
+		Task<Result<LoginResponseDto>> RefreshTokenAsync(RefreshTokenRequestDto refreshTokenRequestDto);
 		Task<Result<UserDto>> AddUserAsync(UserDto userDto);
 		Task<Result<List<UserDto>>> GetAllUsersAsync();
 		Task<Result<UserDto>> GetUserByIdAsync(Guid id);
